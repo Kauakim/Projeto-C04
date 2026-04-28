fecaf83dde649629caeb5b7564121d6ae2b6458c
felposoares <felipe.soares@scoder.com.br>
2026-04-28 13:52:52 -0300

    feat: large switch changed for a if

--- main.cpp
+++ main.cpp
@@ -319,41 +319,13 @@ int menu() {
             cout << "Invalid input. Try again.\n";
             continue;
         }
-
-        switch (option) {
-            case 0:
-                return 0;
-
-            case 1:
-                return 1;
-
-            case 2:
-                return 2;
-
-            case 3:
-                return 3;
-
-            case 4:
-                return 4;
-
-            case 5:
-                return 5;
-
-            case 6:
-                return 6;
-
-            case 7:
-                return 7;
-
-            case 8:
-                return 8;
-
-            case 9:
-                return 9;
-
-            default:
-                cout << "Invalid option.\n";
+        
+        if (option < 0 || option > 9) {
+            cout << "Invalid option. Try again.\n";
+            return menu();
         }
+
+        return option;
     }
 }
 
